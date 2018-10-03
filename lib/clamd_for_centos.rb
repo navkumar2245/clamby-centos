ae1a33da7785117d74288f23e078a48f107e823f
naveen <naveen@anilabsinc.com>
2018-10-03 11:41:53 +0530

    fix amazon linux issue

--- lib/clamd_for_centos.rb
+++ lib/clamd_for_centos.rb
@@ -3,19 +3,18 @@ require "clamd_for_centos/version"
 module ClamdForCentos
   # Your code goes here...
   def self.setup
-    status =  system("sudo yum -y install clamav-server clamav-data clamav-update clamav-filesystem clamav clamav-scanner-systemd clamav-devel clamav-lib clamav-server-systemd")
-    if status == true
+    status =  system("sudo  yum install clamav clamav-scanner-sysvinit clamav-update -y")
+    # if status == true
     file_edit = system("sudo sed -i -e 's/^Example/#Example/' /etc/clamd.d/scan.conf")
-    f = File.open('/etc/clamd.d/scan.conf', 'a')
-    f.write('LocalSocket /var/run/clamd.scan/clamd.sock')
-    f.close
+    file_scanff = system(" echo 'LocalSocket /var/run/clamd.scan/clamd.sock' | sudo tee -a /etc/clamd.d/scan.conf")
+
     file_fresh_clam = system("sudo sed -i -e 's/^Example/#Example/' /etc/freshclam.conf")
     system("sudo freshclam")
-    system("sudo systemctl start clamd@scan")
-    system("sudo systemctl enable clamd@scan")
-    else 
+    system("sudo service clamd.scan start")
+    system("sudo chkconfig clamd.scan on")
+    # else 
     puts "rerun with sudo"
-    end
+    # end
   end
 
 
